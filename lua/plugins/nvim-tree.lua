0f2a184f72d192f1a7bfced78aba297d9ebc852e
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-07-30 23:36:48 -0300

    fix: Remove depreacated code on nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,20 +1,19 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌"
-  },
-  folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
-}
-
+-- vim.g.nvim_tree_icons = {
+--   default = "",
+--   symlink = "",
+--   git = {
+--     unstaged = "",
+--     staged = "S",
+--     unmerged = "",
+--     renamed = "➜",
+--     deleted = "",
+--     untracked = "U",
+--     ignored = "◌"
+--   },
+--   folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
+-- }
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then return end
 
@@ -50,5 +49,23 @@ nvim_tree.setup {
     },
     number = false,
     relativenumber = false
+  },
+  renderer = {
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌"
+        },
+        folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
+      }
+    }
   }
 }
