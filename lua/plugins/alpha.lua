77e1eeaf1017b21b24abde9232ebeca8460205f8
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-07-30 23:34:08 -0300

    style: Add new startting screen image

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -51,18 +51,35 @@ local setzer = {
   [[                ░░                      ]]
 }
 
+local space = {
+  [[]],
+  [[]],
+  [[]],
+  [[██████████████████████████████████████████████████████████████████████████████]],
+  [[██████████    ██████████████  ██████████  ████████████████        ████████████]],
+  [[████████        ██████████████  ██████  ████████████                    ██████]],
+  [[██████            ██████████              ████████                        ████]],
+  [[████    ██    ██    ██████    ██      ██    ██████      ████    ████      ████]],
+  [[████                ████                      ████                        ████]],
+  [[████████  ████  ████████  ██              ██  ██████████    ████    ██████████]],
+  [[██████  ██    ██  ██████  ██  ██████████  ██  ████████    ██    ██    ████████]],
+  [[████  ██  ████  ██  ██████████    ██    ██████████    ████████████████    ████]],
+  [[██████████████████████████████████████████████████████████████████████████████]],
+  [[]],
+  [[]]
+}
 local dashboard = require("alpha.themes.dashboard")
 
-dashboard.section.header.val = slimer;
+dashboard.section.header.val = space;
 
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-  dashboard.button("q", "  Quit Neovim", ":qa<CR>")
+  dashboard.button("p", "  Find project", ":Telescope projects <CR>")
+  -- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  -- dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+  -- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  -- dashboard.button("q", "  Quit Neovim", ":qa<CR>")
 }
 
 local function footer()
