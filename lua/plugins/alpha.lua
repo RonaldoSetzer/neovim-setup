2f2eb777a1b81a811e6ccc164b0f5292e185e44a
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-09-26 23:00:18 -0300

    feat: Add startup link to Git

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -73,9 +73,10 @@ local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = space;
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("p", "  Find project", ":Telescope projects <CR>")
+  dashboard.button("f", "  Find file", "<cmd>Telescope find_files <CR>"),
+  dashboard.button("t", "  Find text", "<cmd>Telescope live_grep <CR>"),
+  dashboard.button("p", "  Find project", "<cmd>Telescope projects <CR>"),
+  dashboard.button("g", "  Git", "<cmd>tab G<CR>"),
   -- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   -- dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   -- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
