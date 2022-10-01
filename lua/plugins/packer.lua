21b4d5e22f8747cfbb02e6a8335494182ca90efb
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2022-09-30 21:02:00 -0300

    feat: Add github Copilot

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -64,8 +64,9 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
-
+  
   -- snippets
+  use "github/copilot.vim"
   use "L3MON4D3/LuaSnip" -- snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
