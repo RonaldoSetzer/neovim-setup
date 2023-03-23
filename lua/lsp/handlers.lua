28b94dc3833b8ff4b0ec7ab9daf1c5550f1f2e35
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2023-03-22 22:26:17 -0300

    fix(lsp): Remove deprecaed capabilities

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -3,19 +3,19 @@ local M = {}
 -- TODO: backfill this to template
 M.setup = function()
   local signs = {
-    {name = "DiagnosticSignError", text = " ✖"}, {name = "DiagnosticSignWarn", text = " ⚠"},
-    {name = "DiagnosticSignHint", text = " "}, {name = "DiagnosticSignInfo", text = " ℹ"}
+    { name = "DiagnosticSignError", text = " ✖" }, { name = "DiagnosticSignWarn", text = " ⚠" },
+    { name = "DiagnosticSignHint", text = " " }, { name = "DiagnosticSignInfo", text = " ℹ" }
   }
 
   for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, {texthl = sign.name, text = sign.text, numhl = ""})
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
   end
 
   local config = {
     -- disable virtual text
     virtual_text = false,
     -- show signs
-    signs = {active = signs},
+    signs = { active = signs },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
@@ -32,16 +32,16 @@ M.setup = function()
   vim.diagnostic.config(config)
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp
-                                               .with(vim.lsp.handlers.hover, {border = "rounded"})
+      .with(vim.lsp.handlers.hover, { border = "rounded" })
 
   vim.lsp.handlers["textDocument/signatureHelp"] =
-      vim.lsp.with(vim.lsp.handlers.signature_help, {border = "rounded"})
+  vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 end
 
 -- I think that its is not working
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
@@ -53,7 +53,7 @@ local function lsp_highlight_document(client)
 end
 
 local function lsp_keymaps(bufnr)
-  local opts = {noremap = true, silent = true}
+  local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_buf_set_keymap
   keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -78,16 +78,16 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "]g", "<cmd>lua require'gitsigns'.next_hunk()<CR>", opts)
 
   -- Format
-  keymap(bufnr, "n", "<leader>p", "<cmd>lua vim.lsp.buf.formatting_sync()<CR>", opts)
+  keymap(bufnr, "n", "<leader>p", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "sumneko_lua" then client.resolved_capabilities.document_formatting = false end
+  if client.name == "sumneko_lua" then client.server_capabilities.document_formatting = false end
 
-  if client.name == "tsserver" then client.resolved_capabilities.document_formatting = false end
+  if client.name == "tsserver" then client.server_capabilities.document_formatting = false end
 
-  if client.name == "gopls" then client.resolved_capabilities.document_formatting = false end
+  if client.name == "gopls" then client.server_capabilities.document_formatting = false end
 
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
@@ -98,6 +98,6 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then return end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
