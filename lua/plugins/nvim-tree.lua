69cf83a394ceeaede7ecbee9d3181ed4174190af
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-01-20 22:28:07 -0300

    fix: Update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -17,9 +17,6 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then return end
 
-local config_status_ok = pcall(require, "nvim-tree.config")
-if not config_status_ok then return end
-
 local function on_attach(bufnr)
   local api = require('nvim-tree.api')
 
@@ -51,12 +48,12 @@ nvim_tree.setup({
   git = { enable = true, ignore = true, timeout = 500 },
   view = {
     width = 30,
-    hide_root_folder = false,
     side = "left",
     number = false,
     relativenumber = false
   },
   renderer = {
+    root_folder_label = false,
     icons = {
       glyphs = {
         default = "",
