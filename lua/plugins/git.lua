8ad8bd6babb475524459781c80a172402ae04b16
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-08-09 14:15:22 -0300

    fix: update gitsigns config to the latest version

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,31 +3,12 @@ if not status_ok then return end
 
 gitsigns.setup {
   signs = {
-    add = {hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
-    change = {
-      hl = "GitSignsChange",
-      text = "▎",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn"
-    },
-    delete = {
-      hl = "GitSignsDelete",
-      text = "▸",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn"
-    },
-    topdelete = {
-      hl = "GitSignsDelete",
-      text = "▸",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn"
-    },
-    changedelete = {
-      hl = "GitSignsChange",
-      text = "▎",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn"
-    }
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -42,7 +23,7 @@ gitsigns.setup {
     delay = 1000,
     ignore_whitespace = false
   },
-  current_line_blame_formatter_opts = {relative_time = false},
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
@@ -55,5 +36,4 @@ gitsigns.setup {
     row = 0,
     col = 1
   },
-  yadm = {enable = false}
 }
