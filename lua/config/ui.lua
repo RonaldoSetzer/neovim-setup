61373a682b4bb1640349332788c88dc110d227c9
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2025-05-18 00:21:36 -0300

    feat: improve statusline: layout and info

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -2,15 +2,33 @@ local M = {}
 
 M.statusline = function()
 	local statusline = require("mini.statusline")
+	set_hl = function(hl, fg, bg, bold)
+		vim.api.nvim_set_hl(0, hl, { fg = fg, bg = bg, bold = bold })
+	end
 
 	statusline.setup({
 		use_icons = true,
 		set_vim_settings = true,
-	})
+		content = {
+			active = function()
+				local left = table.concat({
+					statusline.section_mode(),
+					statusline.section_git(),
+					statusline.section_fileinfo(),
+				}, " ")
+				local right = table.concat({
+					statusline.section_location(),
+					statusline.section_git_status(),
+					statusline.section_diagnostics(),
+				})
 
-	statusline.section_location = function()
-		return "%3l:%-2v"
-	end
+				return left .. "%=" .. right
+			end,
+			inactive = function()
+				return statusline.section_fileinfo()
+			end,
+		},
+	})
 
 	statusline.section_mode = function()
 		local mode = vim.fn.mode()
@@ -32,23 +50,55 @@ M.statusline = function()
 		local mode_char = mode_map[mode].icon or mode:sub(1, 1)
 		local hl = mode_map[mode].color or "%#MiniStatuslineModeOther#"
 
-		return string.format("%s %s ", hl, mode_char)
-	end
-
-	statusline.section_fileinfo = function()
-		local filename = vim.fn.expand("%:t")
-		local filetype = vim.bo.filetype
-		local modified = vim.bo.modified and " [+]" or ""
-		return string.format("%s [%s]%s", filename, filetype, modified)
+		return string.format("%s %s %s", hl, mode_char, "%*")
 	end
 
 	statusline.section_git = function()
 		local branch = vim.b.gitsigns_head
 		if branch then
-			return " " .. branch
+			return "%#MiniStatuslineGit# " .. branch .. "%*"
 		else
 			return ""
 		end
+	end
+
+	statusline.section_fileinfo = function()
+		local project_dir = vim.fn.getcwd()
+		local filepath = vim.fn.expand("%:p")
+		local relative_path = vim.fn.fnamemodify(filepath, ":." .. project_dir)
+		local filename = vim.fn.expand("%:t")
+
+		local filetype = vim.bo.filetype
+		local modified = vim.bo.modified and "  " or ""
+
+		local icon, icon_color =
+			require("nvim-web-devicons").get_icon_color(filename, vim.fn.expand("%:e"), { default = true })
+
+		vim.api.nvim_set_hl(0, "CustomFileIcon", { fg = icon_color })
+
+		return "%#CustomFileIcon# [" .. filetype .. "] " .. (icon or "") .. " %* ./" .. relative_path .. modified
+	end
+
+	statusline.section_location = function()
+		set_hl("MiniStatuslineLocation", "#8ec07c", "#3c3836", false)
+		return "%#MiniStatuslineLocation# %3l:%-2v %*"
+	end
+
+	statusline.section_git_status = function()
+		local gitsigns = vim.b.gitsigns_status_dict or {}
+		local added = gitsigns.added or 0
+		local changed = gitsigns.changed or 0
+		local removed = gitsigns.removed or 0
+		set_hl("MiniStatuslineGit", "#fabd2f", "#3c3836", false)
+		set_hl("MiniStatuslineGitAdded", "#b8bb26", "#3c3836", true)
+		set_hl("MiniStatuslineGitChanged", "#fabd2f", "#3c3836", true)
+		set_hl("MiniStatuslineGitRemoved", "#fb4934", "#3c3836", true)
+
+		return table.concat({
+			"%#MiniStatuslineGit#  %*" .. "%#MiniStatuslineGitAdded# " .. added .. "%*",
+			"%#MiniStatuslineGitChanged#  " .. changed .. "%*",
+			"%#MiniStatuslineGitRemoved#  " .. removed .. " %*",
+		})
 	end
 
 	statusline.section_diagnostics = function()
@@ -59,19 +109,25 @@ M.statusline = function()
 
 		local result = {}
 		if errors > 0 then
-			table.insert(result, " " .. errors)
+			table.insert(result, "%#MiniStatuslineDiagnosticError#  " .. errors .. " %*")
 		end
 		if warnings > 0 then
-			table.insert(result, " " .. warnings)
+			table.insert(result, "%#MiniStatuslineDiagnosticWarn#  " .. warnings .. " %*")
 		end
 		if info > 0 then
-			table.insert(result, " " .. info)
+			table.insert(result, "%#MiniStatuslineDiagnosticInfo#  " .. info .. " %*")
 		end
 		if hints > 0 then
-			table.insert(result, " " .. hints)
+			table.insert(result, "%#MiniStatuslineDiagnosticHint#  " .. hints .. " %*")
 		end
 
-		return table.concat(result, " ")
+		set_hl("MiniStatuslineDiagnostics", "#83a598", "#3c3836", false)
+		set_hl("MiniStatuslineDiagnosticError", "#3c3836", "#fb4934", true)
+		set_hl("MiniStatuslineDiagnosticWarn", "#3c3836", "#fabd2f", true)
+		set_hl("MiniStatuslineDiagnosticInfo", "#3c3836", "#83a598", true)
+		set_hl("MiniStatuslineDiagnosticHint", "#3c3836", "#b8bb26", true)
+
+		return "%#MiniStatuslineDiagnostics# " .. table.concat(result) .. " %*"
 	end
 end
 
