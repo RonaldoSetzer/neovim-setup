612e9e82fae66eb50c924061e8854f0599a4f1f6
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2025-05-19 16:50:26 -0300

    chore: add telescope ignore

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -7,38 +7,39 @@ local actions = require("telescope.actions")
 local M = {}
 
 M.defaults = {
-  mappings = {
-    i = {
-      ["<C-k>"] = actions.move_selection_previous,
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-    },
-  },
-  prompt_prefix = "❯ ",
-  selection_caret = "❯ ",
-  path_display = { "smart" },
-  layout_config = { prompt_position = "top" },
-  sorting_strategy = "ascending",
-  winblend = 10,
+	mappings = {
+		i = {
+			["<C-k>"] = actions.move_selection_previous,
+			["<C-j>"] = actions.move_selection_next,
+			["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+		},
+	},
+	prompt_prefix = "❯ ",
+	selection_caret = "❯ ",
+	path_display = { "smart" },
+	layout_config = { prompt_position = "top" },
+	sorting_strategy = "ascending",
+	winblend = 10,
+	file_ignore_patterns = { "node_modules", ".git" },
 }
 
 M.pickers = {
-    find_files = { theme = "ivy" },
-    live_grep = { theme = "ivy" },
-    buffers = { theme = "ivy" },
-    help_tags = { theme = "ivy" },
-    keymaps = { theme = "ivy" },
-    lsp_definitions = { theme = "ivy" },
-    lsp_references = { theme = "ivy" },
+	find_files = { theme = "ivy", hidden = true },
+	live_grep = { theme = "ivy" },
+	buffers = { theme = "ivy" },
+	help_tags = { theme = "ivy" },
+	keymaps = { theme = "ivy" },
+	lsp_definitions = { theme = "ivy" },
+	lsp_references = { theme = "ivy" },
 }
 
 M.extensions = {
-  fzf = {
-    fuzzy = true,
-    override_generic_sorter = true,
-    override_file_sorter = true,
-    case_mode = "smart_case",
-  },
+	fzf = {
+		fuzzy = true,
+		override_generic_sorter = true,
+		override_file_sorter = true,
+		case_mode = "smart_case",
+	},
 }
 
 -- ╭─────────────────────────────────────────────────────╮
@@ -46,19 +47,22 @@ M.extensions = {
 -- ╰─────────────────────────────────────────────────────╯
 
 M.console_logs = function()
-    local results = vim.fn.systemlist("rg --no-heading --with-filename --line-number --column console\\.log")
+	local results = vim.fn.systemlist("rg --no-heading --with-filename --line-number --column console\\.log")
 
-    if vim.tbl_isempty(results) then
-      vim.notify("No console.log found in project", vim.log.levels.INFO)
-      return
-    end
-  
-    require("telescope.pickers").new({}, {
-      prompt_title = "console.log Files",
-      finder = require("telescope.finders").new_table { results = results },
-      sorter = require("telescope.config").values.generic_sorter({}),
-      previewer = require("telescope.config").values.grep_previewer({}),
-    }):find()
-  end
+	if vim.tbl_isempty(results) then
+		vim.notify("No console.log found in project", vim.log.levels.INFO)
+		return
+	end
+
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "console.log Files",
+			finder = require("telescope.finders").new_table({ results = results }),
+			sorter = require("telescope.config").values.generic_sorter({}),
+			previewer = require("telescope.config").values.grep_previewer({}),
+		})
+		:find()
+end
 
 return M
+
