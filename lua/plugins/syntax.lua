f2ddb1d95c159b5071f8e5ff16f5d8491aeb5c3e
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2025-05-19 21:17:05 -0300

    feat: improve navigation code with treesitter

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -6,6 +6,11 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/playground",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
 			ensure_installed = {
@@ -35,6 +40,56 @@ return {
 			indent = {
 				enable = true,
 				disable = { "yaml" },
+			},
+			textobjects = {
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["[f"] = "@function.outer",
+						["[i"] = "@conditional.outer",
+						["[s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+						["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+					},
+					goto_previous_start = {
+						["]f"] = "@function.outer",
+						["]i"] = "@conditional.outer",
+						["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+					},
+					goto_next = {
+						["[F"] = "@function.outer",
+						["[I"] = "@conditional.outer",
+						["[S"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+						["[Z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+					},
+					goto_previous = {
+						["]F"] = "@function.outer",
+						["]I"] = "@conditional.outer",
+						["]S"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+						["]Z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+					},
+				},
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ai"] = "@conditional.outer",
+						["ii"] = "@conditional.inner",
+						["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						["is"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						["az"] = { query = "@fold", query_group = "folds", desc = "Select language fold" },
+						["iz"] = { query = "@fold", query_group = "folds", desc = "Select language fold" },
+					},
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = true,
+				},
 			},
 			highlight = {
 				enable = true,
