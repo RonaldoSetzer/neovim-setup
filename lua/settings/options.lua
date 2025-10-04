2af442e4f143415dabcfc62ecbeaf45e18a895fa
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2025-10-03 23:56:41 -0300

    chore: modify conceallevel value

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -8,42 +8,43 @@ local opt = vim.opt
 -- Geral
 -- -------------------------------------------------------
 
-opt.title         = true
+opt.title = true
 opt.termguicolors = true
-opt.mouse         = 'a'
-opt.clipboard     = 'unnamedplus'
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
 
 -- -------------------------------------------------------
 -- Busca
 -- -------------------------------------------------------
 
 opt.ignorecase = true
-opt.smartcase  = true
-opt.showmatch  = true
+opt.smartcase = true
+opt.showmatch = true
 
 -- -------------------------------------------------------
 -- Completions & UX
 -- -------------------------------------------------------
 
-opt.completeopt = { 'menu', 'menuone', 'preview', 'noselect', 'noinsert' }
-opt.pumheight   = 10
-opt.updatetime  = 300
-opt.cmdheight   = 1
-opt.showmode    = false
+opt.completeopt = { "menu", "menuone", "preview", "noselect", "noinsert" }
+opt.pumheight = 10
+opt.updatetime = 300
+opt.cmdheight = 1
+opt.showmode = false
 
 -- -------------------------------------------------------
 -- Interface
 -- -------------------------------------------------------
 
-opt.number       = true
-opt.cursorline   = true
-opt.wrap         = true
-opt.linebreak    = true
-opt.breakindent  = true
-opt.signcolumn   = 'yes'
-opt.colorcolumn  = '100'
-opt.showtabline  = 0
-opt.scrolloff    = 10
+opt.number = true
+opt.cursorline = true
+opt.wrap = true
+opt.linebreak = true
+opt.breakindent = true
+opt.signcolumn = "yes"
+opt.colorcolumn = "100"
+opt.showtabline = 0
+opt.scrolloff = 10
+opt.conceallevel = 2
 
 -- -------------------------------------------------------
 -- Splits
@@ -56,31 +57,31 @@ opt.splitbelow = true
 -- Indentação
 -- -------------------------------------------------------
 
-opt.expandtab   = true
-opt.tabstop     = 2
-opt.shiftwidth  = 2
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
 vim.bo.smartindent = true
 
 -- -------------------------------------------------------
 -- Arquivos temporários
 -- -------------------------------------------------------
 
-opt.backup      = false
+opt.backup = false
 opt.writebackup = false
-opt.swapfile    = false
+opt.swapfile = false
 
 -- -------------------------------------------------------
 -- Folding
 -- -------------------------------------------------------
 
-opt.foldmethod  = 'expr'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
-opt.foldlevel  = 99
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
 opt.foldlevelstart = 99
 
 -- -------------------------------------------------------
 -- Desativa netrw (usando nvim-tree/mini.files/etc)
 -- -------------------------------------------------------
 
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
